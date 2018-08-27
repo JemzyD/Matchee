cfb0d64b4d32cf56b78be782247167f98c44462a
JemzyD <jemima.ly.lim@gmail.com>
2018-08-27 20:21:41 +0800

    vinny

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.action_cable.url = 'https://floating-retreat-48742.herokuapp.com/'
-  #  config.action_cable.allowed_request_origins = [ 'https://floating-retreat-48742.herokuapp.com/', /https:\/\/floating-retreat-48742.herokuapp.com.*/ ]
+  # config.action_cable.url = 'https://floating-retreat-48742.app.com/'
+  #  config.action_cable.allowed_request_origins = [ 'herokuapp.com/', /https:\/\/herokuapp.com.*/ ]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "third-project_#{Rails.env}"
-  config.action_mailer.default_url_options = { :host => 'floating-retreat-48742.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
